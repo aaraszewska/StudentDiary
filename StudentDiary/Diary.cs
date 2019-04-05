dbba0bca2a66b96db71411e6833da7a180df9872
Anna <aaraszewska@gmail.com>
2019-04-05 21:57:38 +0100

    second commit

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        //stan pola klasy
+        List<float> ratings = new List<float>();// zainicjowanie obiektu
+
+        // zachowania metody
+        public void AddRating(float rating) //dodawanie oceny do dzienniczka
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverange()
+        {
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();  // count na liscie daje ilosc elementow 
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
--- StudentDiary/Program.cs
+++ StudentDiary/Program.cs
@@ -10,6 +10,31 @@ namespace StudentDiary
     {
         static void Main(string[] args)
         {
+            Diary diary = new Diary();
+            //diary.AddRating(5);
+            //diary.AddRating(8.5f);
+            //diary.AddRating(4.7f);
+            //float avg = diary.CalculateAverange();
+            //float max = diary.GiveMaxRating();
+            //float min = diary.GiveMinRating();
+
+            for (; ; )
+            {
+                Console.WriteLine(  "podaj ocene z zakresu 1-10");
+                float rating = float.Parse(Console.ReadLine());
+                
+
+                if(rating == 11)
+                {
+                    break;
+                }
+                diary.AddRating(rating);
+            }
+            Console.WriteLine("Srednia twoich ocen to:" + diary.CalculateAverange());
+            Console.WriteLine("max ocena to:" + diary.GiveMaxRating());
+            Console.WriteLine("min ocena to:" + diary.GiveMinRating());
+
+            Console.ReadKey();
         }
     }
 }
